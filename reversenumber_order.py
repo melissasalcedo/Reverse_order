159ca6531610e3c11219d9b6d746283a65cc85da
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-23 01:12:12 +0800

    Pseudocode and function

---
+++ reversenumber_order.py
@@ -0,0 +1,10 @@
+# Print the reverse number of the given list
+
+# Pseudocode
+# Function
+def reverse_numbers(digits):
+    reversed_string = str(digits)[::-1]
+
+    reverse_numbers = ' '.join(reversed_string)
+    print(reverse_numbers)
+# Number
